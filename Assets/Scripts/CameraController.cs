f9981270a54737e47f39f0801286d85bfa38bb64
unknown <mnjy@mit.edu>
2015-04-13 00:15:31 -0400

    Basic scene with 1st person view.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public PlayerController player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+
+	void FixedUpdate() {
+		float y = Input.GetAxis ("Horizontal");
+		///float x = -Input.GetAxis ("Vertical");
+		float angularSpeed = player.angularSpeed;
+
+		float x = 0.0f;
+//		float y = 0.0f;
+
+//		if (Input.GetKey ("a")) { ///left
+//			y = -1.0f;
+//		}
+//		if (Input.GetKey ("d")) { ///right
+//			y = 1.0f;
+//		}
+		if (Input.GetKey ("w")) { ///up
+			x = -1.0f;
+		}
+		if (Input.GetKey ("s")) { ///down
+			x = 1.0f;
+		}
+
+		///if (x + transform.rotation.x > 0) { /// don't let it get below ground (x ~ 0)
+		///	x = -transform.rotation.x;
+		///}
+
+		transform.Rotate(x * angularSpeed * Time.deltaTime, 0.0f, 0.0f, Space.Self);
+		transform.Rotate (0.0f, y * angularSpeed * Time.deltaTime, 0.0f);///, Space.World);
+	}
+	
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+		///transform.rotation = player.transform.rotation;
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float angularSpeed;
+	private int count;
+
+	void Start ()
+	{
+		count = 0;
+	}
+
+	void FixedUpdate() 
+	{
+		///FORWARD MOTION
+//		float moveForward = Input.GetAxis ("Vertical");
+		float moveForward = 0.0f;
+		if (Input.GetKey ("up")) { ///up
+			moveForward = 1.0f;
+		}
+		if (Input.GetKey ("down")) { ///down
+			moveForward = -1.0f;
+		}
+		Vector3 movement = new Vector3(0.0f, 0.0f, moveForward);
+		transform.Translate (movement * speed * Time.deltaTime);
+		///GetComponent<Rigidbody>().AddRelativeForce(movement * speed * Time.deltaTime);
+
+		///RIGHT/LEFT ROTATION
+		float rotate_y = Input.GetAxis ("Horizontal");
+		transform.Rotate (0.0f, rotate_y * angularSpeed * Time.deltaTime, 0.0f);///, Space.World);
+
+		///UP/DOWN ROTATION
+		float x = 0.0f;
+		if (Input.GetKey ("w")) { ///up
+			x = -1.0f;
+		}
+		if (Input.GetKey ("s")) { ///down
+			x = 1.0f;
+		}
+		transform.Rotate (x * angularSpeed * Time.deltaTime, 0.0f, 0.0f);///, Space.World);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "PickUp"){
+			other.gameObject.SetActive(false);
+			count += 1;
+		}
+	}
+}
