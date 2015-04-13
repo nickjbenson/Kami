0d8494f1f52a25aeb1841eefa67b1c2bd574acf8
unknown <mnjy@mit.edu>
2015-04-13 01:50:17 -0400

    Added some fishy things

---
+++ Assets/Scripts/FishMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		///speed = Random.Range (100, 500);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody> ().AddRelativeForce (Vector3.forward * speed * Time.deltaTime);
+	}
+}
