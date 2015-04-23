b963cea1461c7a85463dde64786cb5a4ac03100d
Nick Benson <nickbenson@Nicks-MacBook-Pro.local>
2015-04-23 17:43:59 -0400

    Added Oculus and Leap Motion stuff and demo oculus/lm scene.

---
+++ Assets/Critters/OculusReticle.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OculusReticle : MonoBehaviour {
+
+	public Transform looker;
+
+	public float maxDistance = 1000.0f;
+
+	private Transform currentTarget = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public bool HasTarget() {
+		return currentTarget != null;
+	}
+
+	public Transform Target {
+		get {
+			return currentTarget;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// RayCast logic. Sets the current target based on raycast,
+		// then moves the reticle to the target.
+		RaycastHit hit;
+		int layerMask = 1 << 8;
+		layerMask = ~layerMask;
+		if (Physics.Raycast (looker.position, looker.forward, out hit, maxDistance, layerMask)) {
+			currentTarget = hit.transform;
+		} else {
+			currentTarget = null;
+		}
+		this.transform.position = looker.position + looker.forward * hit.distance;
+	}
+}
---
+++ Assets/Critters/PushPuller.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushPuller : MonoBehaviour {
+
+	public HandController handController;
+	public OculusReticle reticle;
+	public float maxMovementDelta = 0.05f;
+	public float minDistance = 1.0f;
+
+	private HandModel[] hands;
+	private float force_magnitude = 0.0f;	
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		hands = handController.GetAllGraphicsHands ();
+
+		force_magnitude = 0.0f;
+		foreach (HandModel hand in hands){
+			// Force magnitude becomes more negative the more hands you have
+			// facing the object. Hands toward object = push away, and vice-versa.
+			force_magnitude -= Vector3.Dot (hand.GetPalmNormal(), reticle.looker.forward);
+		}
+
+		if (reticle.HasTarget ()) {
+			Transform target = reticle.Target;
+			float distance = Vector3.Distance (reticle.Target.position, this.transform.position);
+			if (distance >= minDistance || force_magnitude < 0) {
+				target.position = Vector3.MoveTowards (target.position, this.transform.position,
+			                                              	force_magnitude * maxMovementDelta);
+			}
+		}
+	
+	}
+
+	public float ForceMagnitude {
+		get {
+			return force_magnitude;
+		}
+	}
+}
